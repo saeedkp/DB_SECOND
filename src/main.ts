0e34d309f748917a1aa7cbffae9289877ecb7eb4
saeedkp <kp_saeed1@yahoo.com>
2021-04-07 04:11:04 +0430

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
+import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+      .setTitle('DB LAB 2')
+      .setDescription('DB LAB 2 API description')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
