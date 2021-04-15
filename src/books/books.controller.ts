a8591db4b903901e574978c7dcd4840e91965a47
saeedkp <kp_saeed1@yahoo.com>
2021-04-15 22:17:55 +0430

    Add swagger documentation for books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Header } from '@nestjs/common';
 import { BooksService } from './books.service';
 import CreateBookDto from './dto/create-book.dto';
+import {ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 
 @Controller('book')
 export default class BooksController {
   constructor(private readonly bookServices: BooksService) {}
   
-  @Post()
+  @Header('Content-Type', 'application/json')
+  @ApiResponse({ status: 200, description: "To add new book to database" })
+  @Post('post')
   postUser( @Body() book: CreateBookDto) {
         return this.bookServices.insert(book);
   }
+
+  @ApiResponse({ status: 200, description: "To get all books" })
   @Get()
   getAll() {
     return this.bookServices.getAllBooks();
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,11 @@
+import { Length , IsOptional, Min, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export default class CreateBookDto {
+    @ApiProperty({description:"Enter the name of the book", type: "string"})
     readonly name: string;
+    @ApiProperty({description:"Enter the book's owner ID", type: "number"})
     readonly userID: number;
+    @ApiProperty({description:"Enter IDs of book's genres", type: "array", items: {type: "number"}})
     readonly genreIDs: number[];
   }
