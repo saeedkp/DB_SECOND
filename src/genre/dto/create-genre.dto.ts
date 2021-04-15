accbe9f68fece7cc1410aa42a4f39947e40c0a02
saeedkp <kp_saeed1@yahoo.com>
2021-04-15 22:24:03 +0430

    Add swagger documentation for genre

--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,3 +1,7 @@
+import { Length , IsOptional, Min, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export default class CreateGenreDto {
+  @ApiProperty({description:"Enter the name of the genre.", type: "string"})
     readonly type: string;
   }
--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Header } from '@nestjs/common';
 import GenreServices from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
+import {ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 
 @Controller('genre')
 export default class GenreController {
   constructor(private readonly genreServices: GenreServices) {}
+  @ApiResponse({ status: 200, description: "To add new genre" })
   @Post('post')
   postGenre( @Body() genre: CreateGenreDto) {
     return this.genreServices.insert(genre);
   }
+  @ApiResponse({ status: 200, description: "To get all the genres" }) 
   @Get()
   getAll() {
     return this.genreServices.getAllGenre();
