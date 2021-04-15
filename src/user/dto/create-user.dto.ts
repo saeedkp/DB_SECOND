8246803a8c68a334cb168787048bbc164b0f0238
saeedkp <kp_saeed1@yahoo.com>
2021-04-15 20:20:06 +0430

    Add swagger documentation for user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { Length , IsOptional, Min, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export default class CreateUserDto {
+    @Length(3, 10)
+    @ApiProperty({description:'Enter the name of the user > ', type: "string", minLength: 3, maxLength: 10})
     readonly name: string;
+
+    @ApiProperty({description:"Enter the user's books >", type: "array", items: {type: "number"}})
     readonly books: number[] ;
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,29 @@
-import { Body, Controller, Get, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post, Put, Header } from '@nestjs/common';
 import { UserServices } from './user.service';
 import CreateUserDto from './dto/create-user.dto';
+import {ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly usersServices: UserServices) {}
 
 //'postUser()' will handle the creating of new User
+  @Header('Content-Type', 'application/json')
+  @ApiResponse({ status: 200, description: "Adds new user to database" })
   @Post('post')
   postUser( @Body() user: CreateUserDto) {
     return this.usersServices.insert(user);
   }
 // 'getAll()' returns the list of all the existing users in the database
+  @ApiResponse({ status: 200, description: "To get all the users" })
   @Get()
   getAll() {
     return this.usersServices.getAllUsers();
   }
 
 //'getBooks()' return all the books which are associated with the user 
-// provided through 'userID' by the request  
+// provided through 'userID' by the request
+  @ApiResponse({ status: 200, description: "To get all books of a user with a specific ID which comes with the request" })  
   @Get('books')
   getBooks( @Body('userID', ParseIntPipe) userID: number ) {
     return this.usersServices.getBooksOfUser(userID);
