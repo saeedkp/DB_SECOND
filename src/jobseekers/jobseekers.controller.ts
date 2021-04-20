3fdcef1d01f1e4a4c2211fe2ba91a25a15cf22fe
saeedkp <kp_saeed1@yahoo.com>
2021-04-20 10:53:55 +0430

    Add insert, delete, update and some other functions for freelancer

--- src/jobseekers/jobseekers.controller.ts
+++ src/jobseekers/jobseekers.controller.ts
@@ -4,6 +4,8 @@ import { JobseekersService } from './jobseekers.service';
 import {ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import UpdateEmployerDto from './dto/update-employer.dto';
 import { Public } from '../public.decorator';
+import CreateFreelancerDto from './dto/create-freelancer.dto';
+import UpdateFreelancerDto from './dto/update-freelancer.dto';
 
 @Controller('jobseekers')
 export class JobseekersController {
@@ -12,35 +14,72 @@ export class JobseekersController {
     @Header('Content-Type', 'application/json')
     @ApiResponse({ status: 200, description: "Adds new employer to database" })
     @Public()
-    @Post('employerpost')
-    postEmployer( @Body() user: CreateEmployerDto) {
-        return this.jobseekersServices.insert(user);
+    @Post('employer/post')
+    postEmployer( @Body() employer: CreateEmployerDto) {
+        return this.jobseekersServices.insertEmployer(employer);
     }
 
     @ApiResponse({ status: 200, description: "To get all the employers" })
     @Public()
-    @Get('employers')
-    getAll() {
+    @Get('employer/get')
+    getAllEmp() {
         return this.jobseekersServices.getAllEmployers();
     }
 
     @ApiResponse({ status: 200, description: "To get all projects of an employer with a specific ID which comes with the request" })  
-    @Get('employerprojects')
-    getBooks( @Body('employerId', ParseIntPipe) employerId: number ) {
+    @Get('employer/projects')
+    getProjectsEmp( @Body('employerId', ParseIntPipe) employerId: number ) {
         return this.jobseekersServices.getProjectsOfEmployer(employerId);
     }
 
     @ApiResponse({ status: 200, description: "To delete an employer" })
     @ApiQuery({name: 'employerId', required: true, type: Number, description :`id of employer which is being deleted`})
-    @Delete('employerdelete')
-    deleteEmployer(@Query('employerId') employerId) {
-        return this.jobseekersServices.delete(employerId);
+    @Delete('employer/delete')
+    deleteEmp(@Query('employerId') employerId) {
+        return this.jobseekersServices.deleteEmployer(employerId);
     }
 
     @ApiResponse({ status: 200, description: "To update an employer" })
-    @Put('employerupdate')
-    updateUser(@Body() employer: UpdateEmployerDto) {
-        return this.jobseekersServices.update(employer);
+    @Put('employer/update')
+    updateEmp(@Body() employer: UpdateEmployerDto) {
+        return this.jobseekersServices.updateEmployer(employer);
+    }
+
+
+
+
+    @Header('Content-Type', 'application/json')
+    @ApiResponse({ status: 200, description: "Adds new freelancer to database" })
+    @Public()
+    @Post('freelancer/post')
+    postFreelancer( @Body() freelancer: CreateFreelancerDto) {
+        return this.jobseekersServices.insertFreelancer(freelancer);
+    }
+
+    @ApiResponse({ status: 200, description: "To get all the freelancers" })
+    @Public()
+    @Get('freelancer/get')
+    getAllFree() {
+        return this.jobseekersServices.getAllFreelancers();
+    }
+
+    @ApiResponse({ status: 200, description: "To get all projects of a freelancer with a specific ID which comes with the request" })  
+    @Get('freelancer/projects')
+    getProjectsfree( @Body('freelancerId', ParseIntPipe) freelancerId: number ) {
+        return this.jobseekersServices.getProjectsOfFreelancer(freelancerId);
+    }
+
+    @ApiResponse({ status: 200, description: "To delete a freelancer" })
+    @ApiQuery({name: 'freelancerId', required: true, type: Number, description :`id of freelancer which is being deleted`})
+    @Delete('freelancer/delete')
+    deleteFree(@Query('freelancerId') freelancerId) {
+        return this.jobseekersServices.deleteFreelancer(freelancerId);
+    }
+
+    @ApiResponse({ status: 200, description: "To update a freelancer" })
+    @Put('freelancer/update')
+    updateFree(@Body() freelancer: UpdateFreelancerDto) {
+        return this.jobseekersServices.updateFreelancer(freelancer);
     }
 
 
--- src/jobseekers/jobseekers.service.ts
+++ src/jobseekers/jobseekers.service.ts
@@ -5,11 +5,14 @@ import CreateEmployerDto from './dto/create-employer.dto';
 import {getConnection} from "typeorm";
 import UpdateBookDto from 'src/Books/dto/update-book.dto';
 import UpdateEmployerDto from './dto/update-employer.dto';
+import CreateFreelancerDto from './dto/create-freelancer.dto';
+import FreelancerEntity from 'src/db/freelancer.entity';
+import UpdateFreelancerDto from './dto/update-freelancer.dto';
 
 @Injectable()
 export class JobseekersService {
 
-    async insert(employerDetails: CreateEmployerDto): Promise<EmployerEntity> {
+    async insertEmployer(employerDetails: CreateEmployerDto): Promise<EmployerEntity> {
         const employerEntity: EmployerEntity = EmployerEntity.create();
         const { firstname, lastname, username, password, email, phone_number} = employerDetails;
         employerEntity.firstname = firstname;
@@ -32,13 +35,13 @@ export class JobseekersService {
         return employer.projects;
       }
 
-    async delete(employerId: number): Promise<EmployerEntity> {
+    async deleteEmployer(employerId: number): Promise<EmployerEntity> {
           const employer = await EmployerEntity.findOne(employerId);
           await employer.remove();
           return employer;
       }
 
-    async update(employerDetails: UpdateEmployerDto): Promise<EmployerEntity> {
+    async updateEmployer(employerDetails: UpdateEmployerDto): Promise<EmployerEntity> {
         const { id, firstname, lastname, username, password, email, phone_number} = employerDetails;
         const employer = await EmployerEntity.findOne(id);
         if(employer != undefined) {
@@ -53,8 +56,55 @@ export class JobseekersService {
         return employer;
     }
 
-    async findOne(username: string): Promise<EmployerEntity | undefined> {
-        return await EmployerEntity.findOne({where: {username: username}});
+
+    async insertFreelancer(freelancerDetails: CreateFreelancerDto): Promise<FreelancerEntity> {
+        const freelancerEntity: FreelancerEntity = FreelancerEntity.create();
+        const { firstname, lastname, username, password, email, phone_number, score, account_type} = freelancerDetails;
+        freelancerEntity.firstname = firstname;
+        freelancerEntity.lastname = lastname;
+        freelancerEntity.username = username;
+        freelancerEntity.password = password;
+        freelancerEntity.email = email;
+        freelancerEntity.phone_number = phone_number;
+        freelancerEntity.score = score;
+        freelancerEntity.account_type = account_type;
+        await FreelancerEntity.save(freelancerEntity);
+        return freelancerEntity;
     }
+
+    async getAllFreelancers(): Promise<FreelancerEntity[]> {
+        return await FreelancerEntity.find();
+      }
+
+    async getProjectsOfFreelancer(freelancerID: number): Promise<ProjectEntity[]> {
+        console.log(typeof(freelancerID));
+        const freelancer: FreelancerEntity = await FreelancerEntity.findOne({where: {id: freelancerID}, relations: ['projects']});
+        return freelancer.projects;
+      }
+
+    async deleteFreelancer(freelancerID: number): Promise<FreelancerEntity> {
+          const freelancer = await FreelancerEntity.findOne(freelancerID);
+          await freelancer.remove();
+          return freelancer;
+      }
+
+    async updateFreelancer(freelancerDetails: UpdateFreelancerDto): Promise<FreelancerEntity> {
+        const { id, firstname, lastname, username, password, email, phone_number, score, account_type} = freelancerDetails;
+        const freelancer = await FreelancerEntity.findOne(id);
+        if(freelancer != undefined) {
+            freelancer.firstname = firstname;
+            freelancer.lastname = lastname;
+            freelancer.username = username;
+            freelancer.password = password;
+            freelancer.email = email;
+            freelancer.phone_number = phone_number;
+            freelancer.score = score;
+            freelancer.account_type = account_type;
+            await FreelancerEntity.save(freelancer);
+        }
+        return freelancer;
+    }
+
+
     
 }
