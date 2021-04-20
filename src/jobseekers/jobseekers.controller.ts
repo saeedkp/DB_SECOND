4933bbe6b89c60e107e726ebad50a2a8494a35ad
saeedkp <kp_saeed1@yahoo.com>
2021-04-20 10:01:26 +0430

    Add insert, delete, update and some other functions for employer

--- src/jobseekers/jobseekers.controller.ts
+++ src/jobseekers/jobseekers.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post, Put, Header, Delete, Query } from '@nestjs/common';
+import CreateEmployerDto from './dto/create-employer.dto';
+import { JobseekersService } from './jobseekers.service';
+import {ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
+import UpdateEmployerDto from './dto/update-employer.dto';
+import { Public } from '../public.decorator';
 
 @Controller('jobseekers')
-export class JobseekersController {}
+export class JobseekersController {
+    constructor(private readonly jobseekersServices: JobseekersService) {}
+
+    @Header('Content-Type', 'application/json')
+    @ApiResponse({ status: 200, description: "Adds new employer to database" })
+    @Public()
+    @Post('employerpost')
+    postEmployer( @Body() user: CreateEmployerDto) {
+        return this.jobseekersServices.insert(user);
+    }
+
+    @ApiResponse({ status: 200, description: "To get all the employers" })
+    @Public()
+    @Get('employers')
+    getAll() {
+        return this.jobseekersServices.getAllEmployers();
+    }
+
+    @ApiResponse({ status: 200, description: "To get all projects of an employer with a specific ID which comes with the request" })  
+    @Get('employerprojects')
+    getBooks( @Body('employerId', ParseIntPipe) employerId: number ) {
+        return this.jobseekersServices.getProjectsOfEmployer(employerId);
+    }
+
+    @ApiResponse({ status: 200, description: "To delete an employer" })
+    @ApiQuery({name: 'employerId', required: true, type: Number, description :`id of employer which is being deleted`})
+    @Delete('employerdelete')
+    deleteEmployer(@Query('employerId') employerId) {
+        return this.jobseekersServices.delete(employerId);
+    }
+
+    @ApiResponse({ status: 200, description: "To update an employer" })
+    @Put('employerupdate')
+    updateUser(@Body() employer: UpdateEmployerDto) {
+        return this.jobseekersServices.update(employer);
+    }
+
+
+}
--- src/jobseekers/jobseekers.service.ts
+++ src/jobseekers/jobseekers.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import EmployerEntity from 'src/db/employer.entity';
+import ProjectEntity from 'src/db/project.entity';
+import CreateEmployerDto from './dto/create-employer.dto';
+import {getConnection} from "typeorm";
+import UpdateBookDto from 'src/Books/dto/update-book.dto';
+import UpdateEmployerDto from './dto/update-employer.dto';
 
 @Injectable()
-export class JobseekersService {}
+export class JobseekersService {
+
+    async insert(employerDetails: CreateEmployerDto): Promise<EmployerEntity> {
+        const employerEntity: EmployerEntity = EmployerEntity.create();
+        const { firstname, lastname, username, password, email, phone_number} = employerDetails;
+        employerEntity.firstname = firstname;
+        employerEntity.lastname = lastname;
+        employerEntity.username = username;
+        employerEntity.password = password;
+        employerEntity.email = email;
+        employerEntity.phone_number = phone_number;
+        await EmployerEntity.save(employerEntity);
+        return employerEntity;
+    }
+
+    async getAllEmployers(): Promise<EmployerEntity[]> {
+        return await EmployerEntity.find();
+      }
+
+    async getProjectsOfEmployer(employerID: number): Promise<ProjectEntity[]> {
+        console.log(typeof(employerID));
+        const employer: EmployerEntity = await EmployerEntity.findOne({where: {id: employerID}, relations: ['projects']});
+        return employer.projects;
+      }
+
+    async delete(employerId: number): Promise<EmployerEntity> {
+          const employer = await EmployerEntity.findOne(employerId);
+          await employer.remove();
+          return employer;
+      }
+
+    async update(employerDetails: UpdateEmployerDto): Promise<EmployerEntity> {
+        const { id, firstname, lastname, username, password, email, phone_number} = employerDetails;
+        const employer = await EmployerEntity.findOne(id);
+        if(employer != undefined) {
+            employer.firstname = firstname;
+            employer.lastname = lastname;
+            employer.username = username;
+            employer.password = password;
+            employer.email = email;
+            employer.phone_number = phone_number;
+            await EmployerEntity.save(employer);
+        }
+        return employer;
+    }
+
+    async findOne(username: string): Promise<EmployerEntity | undefined> {
+        return await EmployerEntity.findOne({where: {username: username}});
+    }
+    
+}
