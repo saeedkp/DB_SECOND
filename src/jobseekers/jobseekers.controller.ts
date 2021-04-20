e56d9759303445e951af09020f2c58d9f6a20e70
saeedkp <kp_saeed1@yahoo.com>
2021-04-20 11:33:56 +0430

    Add annotations for making authorization available to jobseekers module

--- src/jobseekers/jobseekers.controller.ts
+++ src/jobseekers/jobseekers.controller.ts
@@ -17,20 +17,21 @@ export class JobseekersController {
 
     @Header('Content-Type', 'application/json')
     @ApiResponse({ status: 200, description: "Adds new employer to database" })
-    @Public()
+    @ApiBearerAuth()
     @Post('employer/post')
     postEmployer( @Body() employer: CreateEmployerDto) {
         return this.jobseekersServices.insertEmployer(employer);
     }
 
     @ApiResponse({ status: 200, description: "To get all the employers" })
-    @Public()
+    @ApiBearerAuth()
     @Get('employer/get')
     getAllEmp() {
         return this.jobseekersServices.getAllEmployers();
     }
 
     @ApiResponse({ status: 200, description: "To get all projects of an employer with a specific ID which comes with the request" })  
+    @ApiBearerAuth()
     @Get('employer/projects')
     getProjectsEmp( @Body('employerId', ParseIntPipe) employerId: number ) {
         return this.jobseekersServices.getProjectsOfEmployer(employerId);
@@ -38,12 +39,14 @@ export class JobseekersController {
 
     @ApiResponse({ status: 200, description: "To delete an employer" })
     @ApiQuery({name: 'employerId', required: true, type: Number, description :`id of employer which is being deleted`})
+    @ApiBearerAuth()
     @Delete('employer/delete')
     deleteEmp(@Query('employerId') employerId) {
         return this.jobseekersServices.deleteEmployer(employerId);
     }
 
     @ApiResponse({ status: 200, description: "To update an employer" })
+    @ApiBearerAuth()
     @Put('employer/update')
     updateEmp(@Body() employer: UpdateEmployerDto) {
         return this.jobseekersServices.updateEmployer(employer);
@@ -54,20 +57,21 @@ export class JobseekersController {
 
     @Header('Content-Type', 'application/json')
     @ApiResponse({ status: 200, description: "Adds new freelancer to database" })
-    @Public()
+    @ApiBearerAuth()
     @Post('freelancer/post')
     postFreelancer( @Body() freelancer: CreateFreelancerDto) {
         return this.jobseekersServices.insertFreelancer(freelancer);
     }
 
     @ApiResponse({ status: 200, description: "To get all the freelancers" })
-    @Public()
+    @ApiBearerAuth()
     @Get('freelancer/get')
     getAllFree() {
         return this.jobseekersServices.getAllFreelancers();
     }
 
     @ApiResponse({ status: 200, description: "To get all projects of a freelancer with a specific ID which comes with the request" })  
+    @ApiBearerAuth()
     @Get('freelancer/projects')
     getProjectsfree( @Body('freelancerId', ParseIntPipe) freelancerId: number ) {
         return this.jobseekersServices.getProjectsOfFreelancer(freelancerId);
@@ -75,12 +79,14 @@ export class JobseekersController {
 
     @ApiResponse({ status: 200, description: "To delete a freelancer" })
     @ApiQuery({name: 'freelancerId', required: true, type: Number, description :`id of freelancer which is being deleted`})
+    @ApiBearerAuth()
     @Delete('freelancer/delete')
     deleteFree(@Query('freelancerId') freelancerId) {
         return this.jobseekersServices.deleteFreelancer(freelancerId);
     }
 
     @ApiResponse({ status: 200, description: "To update a freelancer" })
+    @ApiBearerAuth()
     @Put('freelancer/update')
     updateFree(@Body() freelancer: UpdateFreelancerDto) {
         return this.jobseekersServices.updateFreelancer(freelancer);
@@ -91,14 +97,14 @@ export class JobseekersController {
 
     @Header('Content-Type', 'application/json')
     @ApiResponse({ status: 200, description: "Adds new resume to database" })
-    @Public()
+    @ApiBearerAuth()
     @Post('resume/post')
     postResume( @Body() resume: CreateResumeDto) {
         return this.jobseekersServices.insertResume(resume);
     }
 
     @ApiResponse({ status: 200, description: "To get all the resumes" })
-    @Public()
+    @ApiBearerAuth()
     @Get('resume/get')
     getAllRes() {
         return this.jobseekersServices.getAllResumes();
@@ -106,12 +112,14 @@ export class JobseekersController {
 
     @ApiResponse({ status: 200, description: "To delete a resume" })
     @ApiQuery({name: 'resumeId', required: true, type: Number, description :`id of resume which is being deleted`})
+    @ApiBearerAuth()
     @Delete('resume/delete')
     deleteRes(@Query('resumeId') resumeId) {
         return this.jobseekersServices.deleteResume(resumeId);
     }
 
     @ApiResponse({ status: 200, description: "To update a resume" })
+    @ApiBearerAuth()
     @Put('resume/update')
     updateRes(@Body() resume: UpdateResumeDto) {
         return this.jobseekersServices.updateResume(resume);
@@ -121,14 +129,14 @@ export class JobseekersController {
 
     @Header('Content-Type', 'application/json')
     @ApiResponse({ status: 200, description: "Adds new project to database" })
-    @Public()
+    @ApiBearerAuth()
     @Post('project/post')
     postProject( @Body() project: CreateProjectDto) {
         return this.jobseekersServices.insertProject(project);
     }
 
     @ApiResponse({ status: 200, description: "To get all the projects" })
-    @Public()
+    @ApiBearerAuth()
     @Get('project/get')
     getAllProj() {
         return this.jobseekersServices.getAllProjects();
@@ -136,12 +144,14 @@ export class JobseekersController {
 
     @ApiResponse({ status: 200, description: "To delete a project" })
     @ApiQuery({name: 'ProjectID', required: true, type: Number, description :`id of project which is being deleted`})
+    @ApiBearerAuth()
     @Delete('project/delete')
     deleteProj(@Query('ProjectID') ProjectID) {
         return this.jobseekersServices.deleteProject(ProjectID);
     }
 
     @ApiResponse({ status: 200, description: "To update a project" })
+    @ApiBearerAuth()
     @Put('project/update')
     updateProj(@Body() project: UpdateProjectDto) {
         return this.jobseekersServices.updateProject(project);
--- src/jobseekers/jobseekers.module.ts
+++ src/jobseekers/jobseekers.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JobseekersService } from './jobseekers.service';
 import { JobseekersController } from './jobseekers.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Module({
-  providers: [JobseekersService],
+  providers: [JobseekersService, {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  }],
   controllers: [JobseekersController]
 })
 export class JobseekersModule {}
